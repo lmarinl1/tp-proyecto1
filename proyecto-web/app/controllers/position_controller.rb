92fbee22bceaa90e83b454c23041bd57b2e7efd8
lmarinl1 <lmarinl1@outlook.com>
2018-07-20 23:19:16 -0500

    Almacena y recupera de la base de datos

--- proyecto-web/app/controllers/position_controller.rb
+++ proyecto-web/app/controllers/position_controller.rb
@@ -1,5 +1,6 @@
 class PositionController < ApplicationController
   def getPosition
+  	@positions = Position.last(10)
   end
 
   def save
@@ -7,8 +8,8 @@ class PositionController < ApplicationController
 	    latitud = params[:lat]
 	    longitud = params[:lon]
 	    puts "lon: "+longitud.to_str+" lat: "+latitud.to_str
-	    #@booking = Booking.new(book_param)
-	    if true #@booking.save
+	    @position = Position.new(user: 1,latitud: latitud, longitud: longitud)
+	    if @position.save
 	          format.html { redirect_to position_getPosition_path }
 	    else
 	        flash[:notice_booking_failed] = true
